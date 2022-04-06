ef47ca0a74f53cbe32b7c61e4e77677b0fd1de26
Jager-yoo <y73447jh@gmail.com>
2022-04-06 15:55:09 +0900

    Initial Commit

---
+++ NewOpenMarket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewOpenMarket
+//
+//  Created by 유재호 on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewOpenMarket/NewOpenMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewOpenMarketApp.swift
+//  NewOpenMarket
+//
+//  Created by 유재호 on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct NewOpenMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
