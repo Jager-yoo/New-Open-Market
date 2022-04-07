6a43d7e5658495132778fb19681851eee97809b8
Jager-yoo <y73447jh@gmail.com>
2022-04-08 05:04:43 +0900

    test: 비동기 작업의 테스트 코드 수정
    - 오류가 나는데도 테스트가 통과하는 이슈가 있었는데, 알고보니 비동기 작업은 expectation 을 사용해서 기다려줘야 한다는 걸 알게됨
    - ItemsPage 가져오는 작업에서, 쿼리 순서가 꼬이면 Item 개수가 잘못 들어오는 이슈를 발견함

--- NewOpenMarket/UnitTests/APIRequestTests.swift
+++ NewOpenMarket/UnitTests/APIRequestTests.swift
@@ -9,17 +9,29 @@ import XCTest
 @testable import NewOpenMarket
 
 final class APIRequestTests: XCTestCase {
-
+    
     func test_API_HealthChecker_response_검증() {
+        let expectation = expectation(description: "response 로 OK 들어와야 함")
+        
         API.HealthChecker().execute { result in
             XCTAssertEqual(try! result.get(), "OK")
+            expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 3)
     }
     
     func test_API_FetchItemsPage_response_검증() {
-        API.FetchItemsPage(pageNo: 1, itemsPerPage: 10).execute { result in
-            XCTAssertEqual(try! result.get().pageNo, 1)
-            XCTAssertFalse(try! result.get().hasPrev)
+        let expectation = expectation(description: "item 5개 달라고 하면, 정확하게 5개 넘어와야 함")
+        
+        API.FetchItemsPage(pageNo: 1, itemsPerPage: 5).execute { result in
+            let itemsPage = try! result.get()
+            XCTAssertFalse(itemsPage.hasPrev)
+            XCTAssertEqual(itemsPage.pageNo, 1)
+            XCTAssertEqual(itemsPage.items.count, 5)
+            expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 3)
     }
 }
