1b1bab88e89acc7e8784c1553c64a70bf1323f72
Jager-yoo <y73447jh@gmail.com>
2022-04-16 01:58:22 +0900

    feat: 이미지는 최대 5장까지만 첨부 가능하도록 제한, Alert 출력
    - 앱의 전역에서 사용되는 각종 Alert 를 모아두기 위한 AlertManager 열거형을 [Utility] 폴더에 생성
    - 최대 이미지 개수는 private 타입 프로퍼티(static let)로 선언해두고, 모든 기능을 연동시켜서, 상수의 숫자만 바꾸면 기능, 텍스트, Alert 메시지 전부 연동되도록 함

--- NewOpenMarket/Source/View/ItemAddView.swift
+++ NewOpenMarket/Source/View/ItemAddView.swift
@@ -12,47 +12,15 @@ struct ItemAddView: View {
     @Binding var isActive: Bool
     @State private var isPicking: Bool = false
     @State private var images: [UIImage] = []
+    @State private var isReachedImagesLimit: Bool = false
     
-    private static let maxImagesLimit = 5
+    /// ImagePicker 로 선택할 수 있는 최대 이미지 개수
+    private static let imagesLimit = 5
     
     var body: some View {
         NavigationView {
             ScrollView {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        Button {
-                            isPicking = true
-                        } label: {
-                            Color.clear
-                                .frame(width: 100, height: 100)
-                                .overlay {
-                                    VStack {
-                                        Image(systemName: "camera.fill")
-                                        Text("\(images.count)")
-                                            .foregroundColor(.orange)
-                                        + Text(" / \(Self.maxImagesLimit)")
-                                    }
-                                    .font(.title3)
-                                }
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .strokeBorder()
-                                }
-                        }
-                        .foregroundColor(.secondary)
-                        .sheet(isPresented: $isPicking) {
-                            ImagePicker(selectedImages: $images)
-                        }
-                        
-                        ForEach(images, id: \.self) { image in
-                            Image(uiImage: image)
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                                .cornerRadius(10)
-                        }
-                    }
-                    .padding()
-                }
+                imagesController
                 
                 Divider()
             }
@@ -72,6 +40,55 @@ struct ItemAddView: View {
                 }
             }
         }
+    }
+    
+    private var imagesController: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                Button {
+                    if images.count < Self.imagesLimit {
+                        isPicking = true
+                    } else {
+                        isReachedImagesLimit = true
+                    }
+                } label: {
+                    addImageButton
+                }
+                .foregroundColor(.secondary)
+                .sheet(isPresented: $isPicking) {
+                    ImagePicker(selectedImages: $images)
+                }
+                .alert(isPresented: $isReachedImagesLimit) {
+                    AlertManager.imagesCountReached(Self.imagesLimit)
+                }
+                
+                ForEach(images, id: \.self) { image in
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
+    }
+    
+    private var addImageButton: some View {
+        Color.clear
+            .frame(width: 100, height: 100)
+            .overlay {
+                VStack {
+                    Image(systemName: "camera.fill")
+                    Text("\(images.count)")
+                        .foregroundColor(.orange)
+                    + Text(" / \(Self.imagesLimit)")
+                }
+                .font(.title3)
+            }
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder()
+            }
     }
 }
 
