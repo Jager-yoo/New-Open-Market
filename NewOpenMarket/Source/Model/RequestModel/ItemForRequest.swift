8c49c3faad8ce3e990963b7302e6184e1654aca8
Jager-yoo <y73447jh@gmail.com>
2022-04-18 18:47:16 +0900

    refactor: 상품 등록을 위한 ItemForRequest 타입 구현
    - ImageForRequest 타입은 별도로 만들지 않기로 해서, 파일 삭제함

--- NewOpenMarket/Source/Model/RequestModel/ItemForRequest.swift
+++ NewOpenMarket/Source/Model/RequestModel/ItemForRequest.swift
@@ -11,17 +11,26 @@ struct ItemForRequest: Encodable {
     
     let name: String // 글자 count 3 ~ 100
     let descriptions: String // 글자 count 10 ~ 1,000
-    let price: Double
     let currency: Currency
-    let discountedPrice: Double // 디폴트 0
+    let price: Double
+    let discount: Double // 디폴트 0
     let stock: Int // 디폴트 0
-    let thumbnailID: URL? // 상품 수정할 때만 필요한 프로퍼티
-    let secret: String
+    let thumbnailID: URL? = nil // 상품 수정할 때만 필요한 프로퍼티
+    let secret: String = "#YAkY88u7cpy2K@9" // '4기 예거' vender secret
+    
+    init(name: String, descriptions: String, currency: Currency, price: String, discount: String, stock: String) {
+        self.name = name
+        self.descriptions = descriptions
+        self.currency = currency
+        self.price = Double(price) ?? 0
+        self.discount = Double(discount) ?? 0
+        self.stock = Int(stock) ?? 0
+    }
     
     private enum CodingKeys: String, CodingKey {
         
-        case discountedPrice = "discounted_price"
+        case discount = "discounted_price"
         case thumbnailID = "thumbnail_id"
-        case name, descriptions, price, currency, stock, secret
+        case name, descriptions, currency, price, stock, secret
     }
 }
