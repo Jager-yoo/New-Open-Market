504683c2c3fe29dff60b1442988e06bfbe09a09f
Jager-yoo <y73447jh@gmail.com>
2022-04-26 03:19:36 +0900

    test: 상품 CRUD 통합 테스트 완성
    - 상품(아이템)의 등록 -> 수정 -> 삭제를 위한 Secret 확인 -> 삭제
    - 전체 싸이클을 테스트 코드를 통해 테스트하고, 앱의 잔여물을 남기지 않음

--- NewOpenMarket/UnitTests/APIRequestTests.swift
+++ NewOpenMarket/UnitTests/APIRequestTests.swift
@@ -35,13 +35,13 @@ final class APIRequestTests: XCTestCase {
         XCTAssertNotNil(itemDetail.vendor)
     }
     
-    func test_API_AddItem_FindItemSecret_DeleteItem_통합_검증() async throws {
+    func test_API_AddItem_EditItem_FindItemSecret_DeleteItem_통합_검증() async throws {
         // 서버에 아이템 등록
         let sampleImages = [UIImage(named: "image1_sample")!, UIImage(named: "image2_sample")!]
         let addResponse = try await API.AddItem(
             images: sampleImages,
             name: "테스트",
-            descriptions: "유닛 테스트를 통한 자동 업로드입니다.",
+            descriptions: "유닛 테스트를 통한 자동 업로드입니다",
             currency: .krw,
             price: "1000",
             discount: "",
@@ -49,7 +49,26 @@ final class APIRequestTests: XCTestCase {
         ).asyncExecute()
         
         XCTAssertEqual(addResponse.name, "테스트")
-        XCTAssertEqual(addResponse.description, "유닛 테스트를 통한 자동 업로드입니다.")
+        XCTAssertEqual(addResponse.description, "유닛 테스트를 통한 자동 업로드입니다")
+        
+        // 등록한 아이템 수정
+        let editResponse = try await API.EditItem(
+            itemID: addResponse.id,
+            name: "테스트(수정)",
+            descriptions: "유닛 테스트를 통한 자동 업로드입니다(수정)",
+            currency: .usd,
+            price: "5000",
+            discount: "200",
+            stock: "5"
+        ).asyncExecute()
+        
+        XCTAssertEqual(editResponse.id, addResponse.id)
+        XCTAssertEqual(editResponse.name, "테스트(수정)")
+        XCTAssertEqual(editResponse.description, "유닛 테스트를 통한 자동 업로드입니다(수정)")
+        XCTAssertEqual(editResponse.currency, .usd)
+        XCTAssertEqual(editResponse.price, 5000)
+        XCTAssertEqual(editResponse.discountedPrice, 200)
+        XCTAssertEqual(editResponse.stock, 5)
         
         // 등록한 아이템의 삭제를 위한 Secret 확인
         let secretData = try await API.FindItemSecret(itemID: addResponse.id).asyncExecute()
