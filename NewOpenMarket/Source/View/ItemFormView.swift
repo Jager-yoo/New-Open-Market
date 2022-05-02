d006d336b9efc86cabdb92498c9cb5216dfa0067
Jager-yoo <y73447jh@gmail.com>
2022-05-02 20:45:20 +0900

    refactor: [MVVM] ItemFormViewModel 클래스 분리
    - FocusState 프로퍼티 래퍼의 경우, 뷰모델로 분리하면 'Accessing FocusState's value outside of the body of a View.' 에러가 뜨면서 값이 바뀌질 않았음
    - https://stackoverflow.com/questions/70568987/it-is-possible-to-accessing-focusstates-value-outside-of-the-body-of-a-view
    - 위 링크 참고하여, onChange 수정자 활용해서 문제 해결함

--- NewOpenMarket/Source/View/ItemFormView.swift
+++ NewOpenMarket/Source/View/ItemFormView.swift
@@ -9,51 +9,11 @@ import SwiftUI
 
 struct ItemFormView: View {
     
-    var boundItem: Binding<Item>?
-    @Binding var isActive: Bool
-    @Binding var shouldRefreshList: Bool
-    @FocusState private var isFocused: Field?
-    @State private var images: [UIImage] = []
-    @State private var isPicking: Bool = false
-    @State private var itemName: String = ""
-    @State private var itemPrice: String = ""
-    @State private var itemCurrency: Currency = .krw
-    @State private var itemDiscount: String = ""
-    @State private var itemStock: String = ""
-    @State private var itemDescriptions: String = ""
-    @State private var isSubmitting: Bool = false
-    @State private var isShowingAlert: Bool = false
-    @State private var itemAlerts: ItemAlert? {
-        didSet { isShowingAlert = true } // itemAlerts 를 할당하면 바로 Alert 자동으로 띄움
-    }
-    let editMode: Bool
-    
-    /// ImagePicker 로 선택할 수 있는 최대 이미지 개수
-    private static let imagesLimit: Int = 5
-    private static let boxWidth: CGFloat = 100
-    private static let boxCornerRadius: CGFloat = 10
+    @StateObject private var viewModel: ItemFormViewModel
+    @FocusState private var isFocused: ItemFormViewModel.Field?
     
     init(isActive: Binding<Bool>, editableItem: Binding<Item>? = nil, shouldRefreshList: Binding<Bool>) {
-        self._isActive = isActive
-        self._shouldRefreshList = shouldRefreshList
-        
-        guard let editableItem = editableItem, let uneditableImages = editableItem.wrappedValue.images else {
-            editMode = false
-            return
-        }
-        
-        // 여기부터는 [상품 수정] 목적으로 사용할 경우
-        editMode = true
-        boundItem = editableItem
-        
-        let uneditableImagesData = uneditableImages.compactMap { try? Data(contentsOf: $0.thumbnailURL) }
-        _images = State(wrappedValue: uneditableImagesData.compactMap { UIImage(data: $0) })
-        _itemName = State(wrappedValue: editableItem.wrappedValue.name)
-        _itemPrice = State(wrappedValue: editableItem.wrappedValue.price.asString)
-        _itemCurrency = State(wrappedValue: editableItem.wrappedValue.currency)
-        _itemDiscount = State(wrappedValue: editableItem.wrappedValue.discountedPrice.asString)
-        _itemStock = State(wrappedValue: editableItem.wrappedValue.stock.description)
-        _itemDescriptions = State(wrappedValue: editableItem.wrappedValue.description ?? "")
+        _viewModel = StateObject(wrappedValue: ItemFormViewModel(isActive: isActive, editableItem: editableItem, shouldRefreshList: shouldRefreshList))
     }
     
     var body: some View {
@@ -62,44 +22,16 @@ struct ItemFormView: View {
                 VStack(spacing: 10) {
                     imagesController
                     
-                    TextField("상품 이름", text: $itemName)
-                        .focused($isFocused, equals: .name)
-                        .submitLabel(.next)
-                        .onSubmit {
-                            isFocused = .price
-                        }
-                    HStack {
-                        TextField("상품 가격", text: $itemPrice)
-                            .keyboardType(.numberPad)
-                            .focused($isFocused, equals: .price)
-                            .submitLabel(.next)
-                        Picker("", selection: $itemCurrency) {
-                            Text(Currency.krw.rawValue).tag(Currency.krw)
-                            Text(Currency.usd.rawValue).tag(Currency.usd)
-                        }
-                        .pickerStyle(.segmented)
-                        .frame(width: 120)
-                    }
-                    TextField("할인 금액", text: $itemDiscount)
-                        .keyboardType(.numberPad)
-                        .focused($isFocused, equals: .discount)
-                    TextField("재고 수량", text: $itemStock)
-                        .keyboardType(.numberPad)
-                        .focused($isFocused, equals: .stock)
-                    
-                    TextEditorCustom(text: $itemDescriptions, placeholder: "상품에 대한 자세한 정보를 작성하면 판매확률이 올라가요!", textLimit: 1000, minHeight: 200)
-                        .focused($isFocused, equals: .descriptions)
+                    itemInformationFields
                 }
                 .textFieldStyle(.roundedBorder)
-                .padding(.horizontal)
-            }
+                .padding(.horizontal)            }
             .navigationTitle("상품 등록")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Button {
-                        isActive = false
-                        HapticManager.shared.selection()
+                        viewModel.dismissModal()
                     } label: {
                         Image(systemName: "xmark")
                     }
@@ -116,27 +48,63 @@ struct ItemFormView: View {
             }
         }
         .overlay {
-            FullCoverProgressView(task: $isSubmitting, message: editMode ? "수정 중" : "등록 중")
+            FullCoverProgressView(task: $viewModel.isSubmitting, message: viewModel.editMode ? "수정 중" : "등록 중")
+        }
+    }
+    
+    private var itemInformationFields: some View {
+        Group {
+            TextField("상품 이름", text: $viewModel.itemName)
+                .submitLabel(.next)
+                .onSubmit {
+                    viewModel.focusOn(.price)
+                }
+                .focused($isFocused, equals: .name)
+            
+            HStack {
+                TextField("상품 가격", text: $viewModel.itemPrice)
+                    .keyboardType(.numberPad)
+                    .focused($isFocused, equals: .price)
+                
+                Picker("", selection: $viewModel.itemCurrency) {
+                    Text(Currency.krw.rawValue).tag(Currency.krw)
+                    Text(Currency.usd.rawValue).tag(Currency.usd)
+                }
+                .pickerStyle(.segmented)
+                .frame(width: 120)
+            }
+            
+            TextField("할인 금액", text: $viewModel.itemDiscount)
+                .keyboardType(.numberPad)
+                .focused($isFocused, equals: .discount)
+            
+            TextField("재고 수량", text: $viewModel.itemStock)
+                .keyboardType(.numberPad)
+                .focused($isFocused, equals: .stock)
+            
+            TextEditorCustom(text: $viewModel.itemDescriptions, placeholder: "상품에 대한 자세한 정보를 작성하면 판매확률이 올라가요!", textLimit: 1000, minHeight: 200)
+                .focused($isFocused, equals: .descriptions)
+        }
+        .onChange(of: viewModel.isFocused) { newValue in
+            self.isFocused = newValue
+        }
+        .onChange(of: self.isFocused) { newValue in
+            viewModel.isFocused = newValue
         }
     }
     
     private var imagesController: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
-                if editMode == false {
+                if viewModel.editMode == false {
                     Button {
-                        if images.count < Self.imagesLimit {
-                            isPicking = true
-                        } else {
-                            itemAlerts = .imagesCountLimit
-                        }
-                        HapticManager.shared.selection()
+                        viewModel.activateImagePicker()
                     } label: {
                         addImageBox
                     }
                     .foregroundColor(.secondary)
-                    .sheet(isPresented: $isPicking) {
-                        ImagePicker(selectedImages: $images)
+                    .sheet(isPresented: $viewModel.isPicking) {
+                        ImagePicker(selectedImages: $viewModel.images)
                     }
                 }
                 
@@ -148,63 +116,54 @@ struct ItemFormView: View {
     
     private var addImageBox: some View {
         Color.clear
-            .frame(width: Self.boxWidth, height: Self.boxWidth)
+            .frame(width: viewModel.boxWidth, height: viewModel.boxWidth)
             .overlay {
                 VStack {
                     Image(systemName: "camera.fill")
-                    Text("\(images.count)")
+                    Text("\(viewModel.images.count)")
                         .foregroundColor(.orange)
-                    + Text(" / \(Self.imagesLimit)")
+                    + Text(" / \(viewModel.imagesLimit)")
                 }
                 .font(Font.system(size: 20))
             }
             .overlay {
-                RoundedRectangle(cornerRadius: Self.boxCornerRadius)
+                RoundedRectangle(cornerRadius: viewModel.boxCornerRadius)
                     .strokeBorder()
             }
     }
     
     private var selectedImageBoxes: some View {
-        ForEach(images.indices, id: \.self) { index in
-            Image(uiImage: images[index])
+        ForEach(viewModel.images.indices, id: \.self) { index in
+            Image(uiImage: viewModel.images[index])
                 .resizable()
                 .scaledToFill()
-                .frame(width: Self.boxWidth, height: Self.boxWidth)
-                .cornerRadius(Self.boxCornerRadius)
+                .frame(width: viewModel.boxWidth, height: viewModel.boxWidth)
+                .cornerRadius(viewModel.boxCornerRadius)
                 .overlay {
                     Button {
-                        images.remove(at: index)
-                        HapticManager.shared.selection()
+                        viewModel.removeImage(at: index)
                     } label: {
                         Image(systemName: "xmark.circle.fill")
                             .resizable()
-                            .frame(width: Self.boxWidth / 5, height: Self.boxWidth / 5)
+                            .frame(width: viewModel.boxWidth / 5, height: viewModel.boxWidth / 5)
                     }
-                    .offset(x: Self.boxWidth / 2, y: -Self.boxWidth / 2)
-                    .disabled(editMode)
+                    .offset(x: viewModel.boxWidth / 2, y: -viewModel.boxWidth / 2)
+                    .disabled(viewModel.editMode)
                 }
         }
     }
     
     private var submitButton: some View {
         Button {
-            isFocused = nil
-            if validateItem() {
-                Task {
-                    editMode ? await editItem() : await addItem()
-                }
-            }
-            HapticManager.shared.selection()
+            viewModel.submitItem()
         } label: {
             Text("완료")
         }
-        .alert("알림", isPresented: $isShowingAlert, presenting: itemAlerts) { alert in
+        .alert("알림", isPresented: $viewModel.isShowingAlert, presenting: viewModel.itemAlerts) { alert in
             Button {
-                if alert == .addItemSuccess || alert == .editItemSuccess {
-                    isActive = false
-                }
+                viewModel.controlAlerts(alert)
             } label: {
-                Text(alert == .addItemSuccess || alert == .editItemSuccess ? "좋아요" : "알겠어요")
+                Text(alert.isSucess ? "좋아요" : "알겠어요")
             }
         } message: { alert in
             Text(alert.message)
@@ -214,11 +173,7 @@ struct ItemFormView: View {
     private var keyboardAddOn: some View {
         HStack {
             Button {
-                if isFocused?.rawValue == Field.first {
-                    isFocused = nil
-                } else {
-                    isFocused = Field(rawValue: (isFocused?.rawValue ?? Field.first) - 1) // 실패하면 nil 할당
-                }
+                viewModel.focusUp()
             } label: {
                 Image(systemName: "chevron.up")
             }
@@ -226,11 +181,7 @@ struct ItemFormView: View {
             Divider()
             
             Button {
-                if isFocused?.rawValue == Field.last {
-                    isFocused = nil
-                } else {
-                    isFocused = Field(rawValue: (isFocused?.rawValue ?? Field.last) + 1) // 실패하면 nil 할당
-                }
+                viewModel.focusDown()
             } label: {
                 Image(systemName: "chevron.down")
             }
@@ -238,143 +189,9 @@ struct ItemFormView: View {
             Spacer()
             
             Button {
-                isFocused = nil
+                viewModel.dismissKeyboard()
             } label: {
                 Image(systemName: "keyboard.chevron.compact.down")
-            }
-        }
-    }
-    
-    private func addItem() async {
-        do {
-            isSubmitting = true
-            _ = try await API.AddItem(
-                images: images,
-                name: itemName,
-                descriptions: itemDescriptions,
-                currency: itemCurrency,
-                price: itemPrice,
-                discount: itemDiscount,
-                stock: itemStock
-            ).asyncExecute()
-            itemAlerts = .addItemSuccess
-            shouldRefreshList = true // ListView 에 성공 여부 알림
-            isSubmitting = false
-        } catch {
-            print("⚠️ AddItem 통신 중 에러 발생! -> \(error)")
-            itemAlerts = .addItemFail
-            isSubmitting = false
-        }
-    }
-    
-    private func editItem() async {
-        guard let editableItem = boundItem else {
-            return
-        }
-        
-        do {
-            isSubmitting = true
-            let editedItem = try await API.EditItem(
-                itemID: editableItem.id,
-                name: itemName,
-                descriptions: itemDescriptions,
-                currency: itemCurrency,
-                price: itemPrice,
-                discount: itemDiscount,
-                stock: itemStock
-            ).asyncExecute()
-            itemAlerts = .editItemSuccess
-            shouldRefreshList = true
-            isSubmitting = false
-            boundItem?.wrappedValue = editedItem // 모달 뒤에 있는 ItemDetailView 수정
-        } catch {
-            print("⚠️ EditItem 통신 중 에러 발생! -> \(error)")
-            itemAlerts = .editItemFail
-            isSubmitting = false
-        }
-    }
-    
-    private func validateItem() -> Bool {
-        if images.isEmpty {
-            itemAlerts = .emptyImages
-            return false
-        } else if !(3...100).contains(itemName.count) {
-            itemAlerts = .invalidName
-            isFocused = .name
-            return false
-        } else if itemPrice.isEmpty {
-            itemAlerts = .invalidPrice
-            isFocused = .price
-            return false
-        } else if Double(itemDiscount) ?? .zero > Double(itemPrice) ?? .zero {
-            itemAlerts = .invalidDiscount
-            itemDiscount = ""
-            isFocused = .discount
-            return false
-        } else if !(10...1000).contains(itemDescriptions.count) {
-            itemAlerts = .invalidDescriptions
-            isFocused = .descriptions
-            return false
-        }
-        
-        return true
-    }
-}
-
-private extension ItemFormView {
-    
-    enum Field: Int, CaseIterable, Hashable {
-        
-        case name
-        case price
-        case discount
-        case stock
-        case descriptions
-        
-        static var first: Int {
-            .zero
-        }
-        
-        static var last: Int {
-            Self.allCases.count - 1
-        }
-    }
-    
-    enum ItemAlert {
-        
-        case emptyImages
-        case imagesCountLimit
-        case invalidName
-        case invalidPrice
-        case invalidDiscount
-        case invalidDescriptions
-        case addItemFail
-        case addItemSuccess
-        case editItemFail
-        case editItemSuccess
-        
-        var message: String {
-            switch self {
-            case .emptyImages:
-                return "이미지는 최소 1장 첨부해주세요"
-            case .imagesCountLimit:
-                return "이미지는 최대 \(imagesLimit)장까지 첨부할 수 있어요 😅"
-            case .invalidName:
-                return "상품 이름은 3 ~ 100 글자 사이로 입력해주세요"
-            case .invalidPrice:
-                return "상품 가격을 입력해주세요"
-            case .invalidDiscount:
-                return "할인가는 가격을 초과할 수 없어요"
-            case .invalidDescriptions:
-                return "상품 정보는 10 ~ 1,000 글자 사이로 입력해주세요"
-            case .addItemFail:
-                return "상품 등록에 실패했습니다 🥲"
-            case .addItemSuccess:
-                return "상품이 성공적으로 등록됐습니다 🥰"
-            case .editItemFail:
-                return "상품 수정에 실패했습니다 🥲"
-            case .editItemSuccess:
-                return "상품이 성공적으로 수정됐습니다 🥰"
             }
         }
     }
---
+++ NewOpenMarket/Source/ViewModel/ItemFormViewModel.swift
@@ -0,0 +1,273 @@
+//
+//  ItemFormViewModel.swift
+//  NewOpenMarket
+//
+//  Created by 유재호 on 2022/05/02.
+//
+
+import SwiftUI
+
+final class ItemFormViewModel: ObservableObject {
+    
+    let editMode: Bool
+    var boundItem: Binding<Item>?
+    @Binding var isActive: Bool
+    @Binding var shouldRefreshList: Bool
+    @Published var images: [UIImage] = []
+    @Published var isPicking: Bool = false
+    @Published var itemName: String = ""
+    @Published var itemPrice: String = ""
+    @Published var itemCurrency: Currency = .krw
+    @Published var itemDiscount: String = ""
+    @Published var itemStock: String = ""
+    @Published var itemDescriptions: String = ""
+    @Published var isSubmitting: Bool = false
+    @Published var isShowingAlert: Bool = false
+    @Published var itemAlerts: ItemAlert? {
+        didSet {
+            DispatchQueue.main.async {
+                self.isShowingAlert = true // itemAlerts 를 할당하면 바로 Alert 자동으로 띄움
+            }
+        }
+    }
+    @Published var isFocused: Field?
+    
+    /// ImagePicker 로 선택할 수 있는 최대 이미지 개수
+    let imagesLimit: Int = 5
+    let boxWidth: CGFloat = 100
+    let boxCornerRadius: CGFloat = 10
+    
+    init(isActive: Binding<Bool>, editableItem: Binding<Item>? = nil, shouldRefreshList: Binding<Bool>) {
+        self._isActive = isActive
+        self._shouldRefreshList = shouldRefreshList
+        
+        guard let editableItem = editableItem,
+              let uneditableImages = editableItem.wrappedValue.images else {
+            editMode = false
+            return
+        }
+        
+        // 여기부터는 [상품 수정] 목적으로 사용할 경우
+        editMode = true
+        boundItem = editableItem
+        
+        let uneditableImagesData = uneditableImages.compactMap { try? Data(contentsOf: $0.thumbnailURL) }
+        _images = Published(wrappedValue: uneditableImagesData.compactMap { UIImage(data: $0) })
+        _itemName = Published(wrappedValue: editableItem.wrappedValue.name)
+        _itemPrice = Published(wrappedValue: editableItem.wrappedValue.price.asString)
+        _itemCurrency = Published(wrappedValue: editableItem.wrappedValue.currency)
+        _itemDiscount = Published(wrappedValue: editableItem.wrappedValue.discountedPrice.asString)
+        _itemStock = Published(wrappedValue: editableItem.wrappedValue.stock.description)
+        _itemDescriptions = Published(wrappedValue: editableItem.wrappedValue.description ?? "")
+    }
+    
+    func addItem() async {
+        do {
+            DispatchQueue.main.async {
+                self.isSubmitting = true
+            }
+            _ = try await API.AddItem(
+                images: images,
+                name: itemName,
+                descriptions: itemDescriptions,
+                currency: itemCurrency,
+                price: itemPrice,
+                discount: itemDiscount,
+                stock: itemStock
+            ).asyncExecute()
+            DispatchQueue.main.async {
+                self.itemAlerts = .addItemSuccess
+                self.shouldRefreshList = true // ListView 에 성공 여부 알림
+                self.isSubmitting = false
+            }
+        } catch {
+            print("⚠️ AddItem 통신 중 에러 발생! -> \(error)")
+            DispatchQueue.main.async {
+                self.itemAlerts = .addItemFail
+                self.isSubmitting = false
+            }
+        }
+    }
+    
+    func editItem() async {
+        guard let editableItem = boundItem else {
+            return
+        }
+        
+        do {
+            DispatchQueue.main.async {
+                self.isSubmitting = true
+            }
+            let editedItem = try await API.EditItem(
+                itemID: editableItem.id,
+                name: itemName,
+                descriptions: itemDescriptions,
+                currency: itemCurrency,
+                price: itemPrice,
+                discount: itemDiscount,
+                stock: itemStock
+            ).asyncExecute()
+            DispatchQueue.main.async {
+                self.itemAlerts = .editItemSuccess
+                self.shouldRefreshList = true
+                self.isSubmitting = false
+                self.boundItem?.wrappedValue = editedItem // 모달 뒤에 있는 ItemDetailView 수정
+            }
+        } catch {
+            print("⚠️ EditItem 통신 중 에러 발생! -> \(error)")
+            DispatchQueue.main.async {
+                self.itemAlerts = .editItemFail
+                self.isSubmitting = false
+            }
+        }
+    }
+    
+    func submitItem() {
+        dismissKeyboard()
+        if validateItem() {
+            Task {
+                editMode ? await editItem() : await addItem()
+            }
+        }
+        HapticManager.shared.selection()
+    }
+    
+    private func validateItem() -> Bool {
+        if images.isEmpty {
+            itemAlerts = .emptyImages
+            return false
+        } else if !(3...100).contains(itemName.count) {
+            itemAlerts = .invalidName
+            isFocused = .name
+            return false
+        } else if itemPrice.isEmpty {
+            itemAlerts = .invalidPrice
+            isFocused = .price
+            return false
+        } else if Double(itemDiscount) ?? .zero > Double(itemPrice) ?? .zero {
+            itemAlerts = .invalidDiscount
+            itemDiscount = ""
+            isFocused = .discount
+            return false
+        } else if !(10...1000).contains(itemDescriptions.count) {
+            itemAlerts = .invalidDescriptions
+            isFocused = .descriptions
+            return false
+        }
+        
+        return true
+    }
+    
+    func activateImagePicker() {
+        if images.count < imagesLimit {
+            isPicking = true
+        } else {
+            itemAlerts = .imagesCountLimit(imagesLimit)
+        }
+        HapticManager.shared.selection()
+    }
+    
+    func removeImage(at index: Int) {
+        images.remove(at: index)
+        HapticManager.shared.selection()
+    }
+    
+    func controlAlerts(_ alert: ItemAlert) {
+        if alert.isSucess {
+            isActive = false
+        }
+    }
+    
+    func focusOn(_ field: Field) {
+        isFocused = field
+    }
+    
+    func dismissKeyboard() {
+        isFocused = nil
+    }
+    
+    func focusUp() {
+        if isFocused?.rawValue == Field.first {
+            isFocused = nil
+        } else {
+            isFocused = Field(rawValue: (isFocused?.rawValue ?? Field.first) - 1) // 실패하면 nil 할당
+        }
+    }
+    
+    func focusDown() {
+        if isFocused?.rawValue == Field.last {
+            isFocused = nil
+        } else {
+            isFocused = Field(rawValue: (isFocused?.rawValue ?? Field.last) + 1) // 실패하면 nil 할당
+        }
+    }
+    
+    func dismissModal() {
+        isActive = false
+        HapticManager.shared.selection()
+    }
+}
+
+extension ItemFormViewModel {
+    
+    enum Field: Int, CaseIterable, Hashable {
+        
+        case name
+        case price
+        case discount
+        case stock
+        case descriptions
+        
+        static var first: Int {
+            .zero
+        }
+        
+        static var last: Int {
+            Self.allCases.count - 1
+        }
+    }
+    
+    enum ItemAlert: Equatable {
+        
+        case emptyImages
+        case imagesCountLimit(_ limit: Int)
+        case invalidName
+        case invalidPrice
+        case invalidDiscount
+        case invalidDescriptions
+        case addItemFail
+        case addItemSuccess
+        case editItemFail
+        case editItemSuccess
+        
+        var isSucess: Bool {
+            let successAlerts: [ItemAlert] = [.addItemSuccess, .editItemSuccess]
+            return successAlerts.contains(self)
+        }
+        
+        var message: String {
+            switch self {
+            case .emptyImages:
+                return "이미지는 최소 1장 첨부해주세요"
+            case .imagesCountLimit(let limit):
+                return "이미지는 최대 \(limit)장까지 첨부할 수 있어요 😅"
+            case .invalidName:
+                return "상품 이름은 3 ~ 100 글자 사이로 입력해주세요"
+            case .invalidPrice:
+                return "상품 가격을 입력해주세요"
+            case .invalidDiscount:
+                return "할인가는 가격을 초과할 수 없어요"
+            case .invalidDescriptions:
+                return "상품 정보는 10 ~ 1,000 글자 사이로 입력해주세요"
+            case .addItemFail:
+                return "상품 등록에 실패했습니다 🥲"
+            case .addItemSuccess:
+                return "상품이 성공적으로 등록됐습니다 🥰"
+            case .editItemFail:
+                return "상품 수정에 실패했습니다 🥲"
+            case .editItemSuccess:
+                return "상품이 성공적으로 수정됐습니다 🥰"
+            }
+        }
+    }
+}
